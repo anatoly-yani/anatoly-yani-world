67a98eff3a6ebbaec9e764ac84c2610c495a501f
Vanuan <vanuan@gmail.com>
2024-07-15 14:32:58 +0300

    Add templates and styles

---
+++ themes/yani-theme/static/js/quote.js
@@ -0,0 +1,20 @@
+function loadQuote() {
+  fetch('/quotes.json')
+    .then(response => response.json())
+    .then(quotes => {
+      const today = new Date();
+      const start = new Date(today.getFullYear(), 0, 0);
+      const diff = today - start;
+      const oneDay = 1000 * 60 * 60 * 24;
+      const dayOfYear = Math.floor(diff / oneDay);
+      
+      const index = dayOfYear % quotes.length;
+      const quote = quotes[index];
+      
+      document.getElementById('quote-text').innerHTML = quote.text.replace(/\n/g, '<br>');
+      document.getElementById('quote-source').innerHTML = `&laquo;${quote.source}&raquo;`;
+      document.getElementById('quote-link').href = `${quote.link}`;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', loadQuote);
